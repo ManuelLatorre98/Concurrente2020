98f16719cbcf7c08aff5547c81f6fba567cba24d
Manuel Latorre <manulatorre@yahoo.com.ar>
2020-11-17 10:53:33 -0300

    Lectores/Escritores 2.0

--- src/LocksCondicion/Biblioteca.java
+++ src/LocksCondicion/Biblioteca.java
@@ -1,52 +1,70 @@
 package LocksCondicion;
+
 import java.util.concurrent.locks.*;
+
 public class Biblioteca {
 	private Lock lock;
-	private Condition esperanAcceso;
-	private boolean escribiendo=false;
-	private int leyendo=0;
+	private Condition esperanLeer;
+	private Condition esperanEscribir;
+	private boolean escribiendo = false;
+	private int leyendo = 0;
+	private int cantEspLeer = 0;
+	private int cantEspEsc = 0;
+
 	public Biblioteca() {
-		lock= new ReentrantLock(true);
-		this.esperanAcceso= this.lock.newCondition();
+		lock = new ReentrantLock(true);
+		this.esperanLeer = this.lock.newCondition();
+		this.esperanEscribir = this.lock.newCondition();
 	}
-	
+
 	public void leer() {
 		this.lock.lock();
-		while(escribiendo) {
+		while (escribiendo) {
 			try {
-				this.esperanAcceso.await();
-			}catch(InterruptedException e) {}
+				cantEspLeer++;
+				this.esperanLeer.await();
+			} catch (InterruptedException e) {
+			}
 		}
+		cantEspLeer--;
 		leyendo++;
 		this.lock.unlock();
 	}
-	
+
 	public void dejarLeer() {
 		this.lock.lock();
 		leyendo--;
-		if(this.leyendo==0) {
-			this.esperanAcceso.signal();
+		if (this.leyendo == 0 && this.cantEspEsc > 0) {// Si no hay lectores y hay escritores esperando les avisa
+			this.esperanEscribir.signal();// Libero a uno solo
 		}
 		this.lock.unlock();
 	}
-	
+
 	public void escribir() {
 		this.lock.lock();
-		while(escribiendo || leyendo>0) {
+		while (escribiendo || leyendo > 0) {
 			try {
-				this.esperanAcceso.await();
-			}catch(InterruptedException e) {}
+				this.cantEspEsc++;
+				this.esperanEscribir.await();
+			} catch (InterruptedException e) {
+			}
 		}
-		this.escribiendo=true;
+		cantEspEsc--;
+		this.escribiendo = true;
 		this.lock.unlock();
 	}
-	
+
 	public void dejarEscribir() {
 		this.lock.lock();
-		this.escribiendo=false;
-		this.esperanAcceso.signal();
+		this.escribiendo = false;
+		if (this.cantEspLeer > 0) {
+			this.esperanLeer.signalAll();// Si hay lectores esperando los libero a todos para que lean
+		} else {
+			if (this.cantEspEsc > 0) {
+				this.esperanEscribir.signal();// Si no hay lectores libero a un escritor esperando
+			}
+		}
 		this.lock.unlock();
 	}
-	
 
 }
